5ba583f2696cd6e2bd287a3bbf4e3cce27b981ca
Luciano De Gianni <lucdegianni@gmail.com>
2020-10-14 22:09:26 -0700

    First commit.

---
+++ public/js/GameManager.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update,
+    }
+};
+
+var game = new Paser.Game(config);
+
+function preload()
+{
+
+}
+
+function create()
+{
+
+}
+
+function update()
+{
+    
+}
